524b6b1ebffbce3fdadfd9c01c141758bebe2a27
nivesayee <nivesayee@gmail.com>
2025-02-02 23:05:29 +0000

    Homework2

---
+++ Homework2/queries.sql
@@ -0,0 +1,3 @@
+select count(*) from yellow_tripdata where filename like 'yellow_tripdata_2020%';
+select count(*) from green_tripdata where filename like 'green_tripdata_2020%';
+select count(*) from yellow_tripdata where filename like 'yellow_tripdata_2021-03.csv';
